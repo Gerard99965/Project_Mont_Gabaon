46c5d905c4adc98afa203047357eac18c3b923f8
gerard <75065003+Gerard99965@users.noreply.github.com>
2024-02-11 11:11:03 +0100

    Update connection.php

--- dashboard/dashboard/configuration/connection.php
+++ dashboard/dashboard/configuration/connection.php
@@ -2,7 +2,7 @@
   try
   {
   //montgabaon2024.infinityfreeapp.com
-  $bdd = new PDO('mysql:host=localhost;dbname=mont_gabaon', 'root', '');
+  $bdd = new PDO('mysql:host=mysql-montgabaon.alwaysdata.net;dbname=montgabaon_dbname', '347452_mont', 'Sofias@1699');
   }
   catch (Exception $e)
   {
